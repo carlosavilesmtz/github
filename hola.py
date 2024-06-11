b9d91b34bf0278ccf0932f131ce32ef100d5293b
dr.rottengrab <dr.rottengrab@gmail.com>
2024-06-11 13:48:25 +0000

    tercer commuit

--- hola.py
+++ hola.py
@@ -1,2 +1,15 @@
 print("hello")
 print("bye")
+def print_hello():
+    print("hello")
+print_hello()
+def print_bye():
+    print("bye")
+print_bye()
+def print_hello_bye():
+    print_hello()
+    print_bye()
+print_hello_bye()
+print_hello_bye()
+print_hello_bye()
+print_hello_bye()
